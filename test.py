6712d6082490fb6a18b946d61990b17b50c4239f
Ashutosh Mishra <ashutosh.mishra@tekizma.com>
2025-11-07 22:01:02 -0500

    first update

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-print("Hello world")
+from google import genai
+
+#print("Hello world")
+
+# The client gets the API key from the environment variable `GEMINI_API_KEY`.
+client = genai.Client(api_key="YOUR-API-KEY")
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash", contents="Explain how AI works in a few words"
+)
+print(response.text)
