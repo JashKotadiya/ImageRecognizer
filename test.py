3fdd2c38f70b7fdb948497f3bcd7e7bb3ce4b138
JashKotadiya <98498584+JashKotadiya@users.noreply.github.com>
2025-11-07 22:12:45 -0500

    Added API Key

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from google import genai
 #print("Hello world")
 
 # The client gets the API key from the environment variable `GEMINI_API_KEY`.
-client = genai.Client(api_key="YOUR-API-KEY")
+client = genai.Client(api_key="AIzaSyCJmPxUv36qoucvBFQ7-TWDW8kGjRkc_Go")
 
 response = client.models.generate_content(
     model="gemini-2.5-flash", contents="Explain how AI works in a few words"
