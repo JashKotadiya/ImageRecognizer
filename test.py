5c319a5526e2683f73890aeb8c936c5a84058911
BryceKirkwood1001 <kirkwoodbryce19@gmail.com>
2025-11-07 23:28:18 -0500

    api key as env variable

--- test.py
+++ test.py
@@ -1,14 +1,19 @@
 import google.generativeai as genai
 from PIL import Image
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+apiKey = os.getenv("GOOGLE_API_KEY")
 
 # Configure with your API key
-genai.configure(api_key="AIzaSyCJmPxUv36qoucvBFQ7-TWDW8kGjRkc_Go")
+genai.configure(api_key=apiKey)
 
 # Load the multimodal model
 model = genai.GenerativeModel("gemini-2.5-flash")
 
 # Open an image (any supported format)
-img = Image.open("C:/Users/Tanda/Downloads/download.jpg")
+img = Image.open(r"C:\Users\kirkw\My Files\Personal\jawStarePfp.jpg")
 
 # Ask the model about it
 response = model.generate_content(["What is in this image? Only give one word", img])
