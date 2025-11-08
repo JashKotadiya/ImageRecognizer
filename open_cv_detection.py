616229b601e088463a799f4b149faf53b0e9063a
Ashutosh Mishra <ashutosh.mishra@tekizma.com>
2025-11-08 16:00:34 -0500

    cleaned up code

--- open_cv_detection.py
+++ open_cv_detection.py
@@ -2,14 +2,31 @@ import cv2
 
 thres = 0.45 # Threshold to detect object
 
-cap = cv2.VideoCapture (1) cap. set (3,1280) cap. set (4, 720) cap. set (10, 70)
+cap = cv2.VideoCapture (1)
+cap.set(3,1280) 
+cap.set(4, 720)
+cap.set(10, 70)
 
-classNames= [] classFile = 'coco.names' with open(classFile, 'rt') as f: classNames = f.read().rstrip('\n').split('\n')
+classNames= []
+classFile = 'coco.names'
+with open(classFile, 'rt') as f:
+    classNames = f.read().rstrip('\n').split('\n')
 
-configPath = 'ssd mobilenet_v3_large_coco 2020_01_14.pbtxt' weightsPath = 'frozen_inference_graph.pb'
+configPath = 'ssd mobilenet_v3_large_coco 2020_01_14.pbtxt' 
+weightsPath = 'frozen_inference_graph.pb'
 
-net=cv2.dnn DetectionModel (weightsPath, configPath) net.setInputSize(320,320) net.setInputScale (1.0/ 127.5) net.setInputMean((127.5, 127.5, 127.5)) net.set InputSwapRB (True)
+net = cv2.dnnDetectionModel(weightsPath, configPath)
+net.setInputSize(320,320)
+net.setInputScale (1.0/ 127.5)
+net.setInputMean((127.5, 127.5, 127.5))
+net.setInputSwapRB(True)
 
-while True: success, img = cap. read () classIds, confs, bbox = net.detect (img, confThreshold=thres) print (classIds,bbox)
+while True: 
+    success, img = cap.read() 
+    classIds, confs, bbox = net.detect(img, confThreshold=thres)
+    print (classIds,bbox)
 
-if len(classIds) != 0: for classId, confidence, box in zip(classIds.flatten(),confs.flatten(),bbox) : cv2.rectangle(img,box,color=(0,255,0),thickness=2) cv2.putText(img, classNames[classId-1].upper(),(box[0]+10,box[1]+30),
+    if len(classIds) != 0: 
+        for classId, confidence, box in zip(classIds.flatten(),confs.flatten(),bbox):
+            cv2.rectangle(img,box,color=(0,255,0),thickness=2) 
+            cv2.putText(img, classNames[classId-1].upper(),(box[0]+10,box[1]+30))
