ed3a753b47a8fdd53074c282cb5880047f129f4e
Ashutosh Mishra <ashutosh.mishra@tekizma.com>
2025-11-07 23:10:57 -0500

    Hope this works

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ genai.configure(api_key="AIzaSyCJmPxUv36qoucvBFQ7-TWDW8kGjRkc_Go")
 model = genai.GenerativeModel("gemini-2.5-flash")
 
 # Open an image (any supported format)
-img = Image.open("C:/Users/Jash_/Downloads/download.jpeg")
+img = Image.open("C:/Users/ash7m/OneDrive/HackUmass/ImageRecognizer/cat.jpg")
 
 # Ask the model about it
 response = model.generate_content(["What is in this image?", img])
